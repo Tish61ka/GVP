a48e05903ef168b7490f8b8fdae6b885f2b8729b
Tishka <yuriitish@mail.ru>
2022-05-27 09:41:53 +0400

    first commit

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/main.css">
+    <link rel="stylesheet" type="text/css" href="css/jquery.jscrollpane.custom.css" />
+	<link rel="stylesheet" type="text/css" href="css/bookblock.css" />
+	<link rel="stylesheet" type="text/css" href="css/custom.css" />
+	<script src="js/modernizr.custom.79639.js"></script>
+    <title>Gravity Falls</title>
+</head>
+<body>
+<div class="bb-custom-wrapper">
+				<div id="bb-bookblock" class="bb-bookblock">
+				<div class="bb-item" id="item1">
+						<div class="content">
+							<div class="scroller">
+                                <div class="opasity_block"></div>
+								<img src="img/picture.png" alt="">
+							</div>
+						</div>
+					</div>
+					<div class="bb-item" id="item1">
+						<div class="content">
+							<div class="scroller">
+                                <div class="opasity_block"></div>
+								<img src="img/page3.png" alt="">
+							</div>
+						</div>
+					</div>
+					<div class="bb-item" id="item2">
+						<div class="content">
+							<div class="scroller">
+                            <div class="opasity_block"></div>
+                            <img src="img/page3.png" alt="">
+							</div>
+						</div>
+					</div>
+					<div class="bb-item" id="item4">
+						<div class="content">
+							<div class="scroller">
+                            <div class="opasity_block"></div>
+                            <img src="img/page3.png" alt="">
+                        </div>
+						</div>
+					</div>
+					<div class="bb-item" id="item5">
+						<div class="content">
+							<div class="scroller">
+                            <div class="opasity_block"></div>
+                            <img src="img/page3.png" alt="">
+                        </div>
+						</div>
+					</div>
+				</div>
+			</div>
+            <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+		<script src="js/jquery.mousewheel.js"></script>
+		<script src="js/jquery.jscrollpane.min.js"></script>
+		<script src="js/jquerypp.custom.js"></script>
+		<script src="js/jquery.bookblock.js"></script>
+		<script src="js/page.js"></script>
+		<script>
+			$(function() {
+
+				Page.init();
+
+			});
+		</script>
+</body>
+</html>
