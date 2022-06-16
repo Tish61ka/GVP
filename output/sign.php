f4fa7fa60f858f869447bb7e022a3cc35dea7567
Tishka <yuriitish@mail.ru>
2022-06-17 00:29:40 +0400

    фикс

--- output/sign.php
+++ output/sign.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="/css/sign.css">
     <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet">
     <title>Document</title>
